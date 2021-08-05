c5fca22dc6ceb1e3fa8e0c1683eb85bd39dc98c6
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-08-05 13:51:38 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  # newArr = array.pop
+  # last = "and #{array.last}"
+  # newArr << last
+  # if newArr.length == 2 
+  #   newArr.join(" ")
+  # else
+  #   newArr.join(",")
+  # end
+  # newArr
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
